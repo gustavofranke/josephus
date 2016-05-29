b88ae1e0ef63ad2481358ff0c371aeab57c69aeb
Gustavo Franke <franke.gustavo@gmail.com>
2016-05-29 14:28:25 +0100

    add scaladoc

--- src/main/scala/com/gustavo/CountingOut.scala
+++ src/main/scala/com/gustavo/CountingOut.scala
@@ -1,6 +1,11 @@
 package com.gustavo
 
 object CountingOut {
+  /** Returns the place you need to stand in the circle, in order to be the last one.
+    *
+    * @param n number of people in the circle.
+    * @param k step rate.
+    */
   def getPlaceToStandInTheCircle(n: Int, k: Int): Int = {
     require(n >= 1, "\n Usage: number of people in the circle (n) has to be greater than 1")
     require(k >= 0, "\n Usage: step rate (k) has to be greater than 0")
